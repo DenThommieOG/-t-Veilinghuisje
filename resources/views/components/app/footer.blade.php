aab161ef90ac88f3124210a8dad4b38e7ad630e4
DenThommieOG <raemaekersthomas@gmail.com>
2023-12-04 21:58:03 +0100

    css updates + footer

---
+++ resources/views/components/app/footer.blade.php
@@ -0,0 +1,9 @@
+<footer>
+    <h2 class="center">Contactgegevens</h2>
+    <div class="flex">
+        <p>
+        tel:+32 (0) 493945479</p>
+        <p>Email: tveilinghusje@gmail.com</p>
+        <p class="">Openingsuren: Op werkdagen van 18u-22u </p>
+    </div>
+</footer>
